d1f783f3009ecb04259c36c5d41e87cc44a5723c
davidhadhazi <106445603+davidhadhazi@users.noreply.github.com>
2023-03-07 21:59:37 +0100

    gtk calculate button

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,41 @@
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Main (main) where
+module Main where
 
 import Data.GI.Base
 import qualified GI.Gtk as Gtk
--- import Tokens
--- import SyntaxTree
--- import Number
--- import Derivate
--- import Simplification
+import qualified Data.Text as T
+import qualified SyntaxTree as ST
+
+eval :: Gtk.Entry -> Gtk.Entry -> (String -> String) ->IO ()
+eval ent1 ent2 f = do
+    txt <- Gtk.entryGetText ent1
+    let txt' = show $ ST.calculate $ ST.makeSyntax $ T.unpack txt
+    _ <- Gtk.entrySetText ent2 $ T.pack txt'
+    set ent1 [#text := ""]
 
 main :: IO()
 main = do
     _ <- Gtk.init Nothing
 
     win <- new Gtk.Window [#title := "Haskell-Alpha"]
+    #maximize win
     _ <- on win #destroy Gtk.mainQuit
-    #showAll win
 
+    box <- new Gtk.Box [#orientation := Gtk.OrientationVertical, #spacing := 10]
+    #add win box
+
+    entry <- new Gtk.Entry []
+
+    result <- new Gtk.Entry []
+
+    button <- new Gtk.Button [#label := "calculate"]
+    _ <- on button #clicked (eval entry result (show))
+    
+    #add box entry
+    #add box button
+    #add box result
+
+    #showAll win
     Gtk.main
