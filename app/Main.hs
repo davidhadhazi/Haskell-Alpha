67df4a0f7bdd64c730a70f1cf8df8aab80494cc7
davidhadhazi <106445603+davidhadhazi@users.noreply.github.com>
2023-05-17 16:20:56 +0200

    error to images

--- app/Main.hs
+++ app/Main.hs
@@ -18,45 +18,62 @@ import Data.Default.Class
 import Graphics.Rendering.Chart.Backend.Cairo
 
 import Control.Exception
-
 import Graphics.Rendering.Chart.Easy
 
-calc :: (String -> String) -> String -> IO (String)
-calc f str = do
-    let txt = f str
-    return txt
+writeError :: Gtk.Entry -> SomeException -> IO()
+writeError ent _ = Gtk.entrySetText ent $ T.pack "Error"
+
+writeResult :: T.Text -> Gtk.Entry -> (String -> String) -> IO ()
+writeResult txt ent f = do
+    Gtk.entrySetText ent (T.pack (f (T.unpack txt)))
+
 
 eval :: Gtk.Entry -> Gtk.Entry -> (String -> String) -> IO ()
 eval ent1 ent2 f = do
     txt <- Gtk.entryGetText ent1
-    
-    catch (Gtk.entrySetText ent2 (T.pack (f (T.unpack txt)))) handler where
-        handler :: SomeException -> IO ()
-        handler _ = Gtk.entrySetText ent2 $ T.pack "Error"
-
-    -- result <- try (calc f (T.unpack txt)) :: IO (Either SomeException String)
-    -- case result of
-    --     Left _ -> Gtk.entrySetText ent2 $ T.pack "Error"
-    --     Right str -> Gtk.entrySetText ent2 $ T.pack str
-
-    -- genImage ent2
-    -- set ent1 [#text := ""]
+    catch (writeResult txt ent2 f) (writeError ent2)
+    genImage ent2
 
 
-am :: ST.Expression -> Double -> Double
-am expr x = N.toDouble $ ST.calculate $ ST.replace x expr
+am :: ST.Expression -> Double -> IO (Maybe Double)
+am expr x = do
+    result <- try (return (N.toDouble (ST.calculate (ST.replace x expr)))) :: IO (Either SomeException Double)
+    case result of
+        Left _ -> return Nothing
+        Right n -> return $ Just n
 
-signal :: ST.Expression -> [Double] -> [(Double,Double)]
-signal expr = map (\x -> (x, am expr x))
+-- signal :: ST.Expression -> [Double] -> [(Double,Double)]
+-- signal expr = map (\x -> (x, am expr x))
+
+-- genPoint :: ST.Expression -> Double -> IO (Either Double Char)
+-- genPoint expr n = do
+--     x <- (Left (catch (am expr n))) _ 
+--     return x where
+--         handler :: SomeException -> IO (Double)
+--         handler _ = return 0
+
+genPoints :: ST.Expression -> [Double] -> IO ([(Double, Double)])
+genPoints _ [] = do
+    let list = []
+    return list
+genPoints expr (x:xs) = do
+    list <- genPoints expr xs
+    y <- am expr x
+    case y of
+        Nothing -> return list
+        Just d -> return $ (x, d) : list
+
 
 genImage :: Gtk.Entry -> IO ()
 genImage ent = do
     txt <- Gtk.entryGetText ent
     let expr = ST.makeSyntax $ T.unpack txt
+    -- print ""
+    myPoints <- genPoints expr [0.0,0.1..10.0]
 
     toFile def "src/asd.jpeg" $ do
         setColors [opaque blue]
-        plot (line "" ([signal expr ([0.0,0.01 .. 10.0])]))
+        plot (line "" ([myPoints]))
 
 
 main :: IO()
